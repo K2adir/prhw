e7725263a77283d8d69f436e302aeed9070e2083
K2adir <k2adir@gmail.com>
2023-02-15 13:27:31 -0500

    "created person class"

--- LAB-1/lab3Js.js
+++ LAB-1/lab3Js.js
@@ -14,3 +14,59 @@ class Hamster {
     return this.price;
   }
 }
+
+class Person {
+  constructor(
+    name,
+    age = 0,
+    height = 0,
+    weight = 0,
+    mood = 0,
+    hamster = [],
+    bankAccount = 0
+  ) {
+    this.name = name;
+    this.age = age;
+    this.height = height;
+    this.weight = weight;
+    this.mood = mood;
+    this.hamster = hamster;
+    this.bankAccount = bankAccount;
+  }
+
+  getName() {
+    console.log(this.name);
+  }
+  getAge() {
+    console.log(this.age);
+  }
+  getWeight() {
+    console.log(this.weight);
+  }
+
+  getHeight() {
+    console.log(this.height);
+  }
+
+  eat() {
+    this.mood++;
+    this.weight++;
+  }
+
+  exercise() {
+    this.weight--;
+  }
+  ageUp() {
+    this.age++;
+    this.height++;
+    this.weight++;
+    this.mood--;
+    this.bankAccount += 10;
+  }
+
+  buyHamster(hamster) {
+    this.mood += 10;
+    this.bankAccount -= hamster.getPrice();
+    this.hamster.push(hamster);
+  }
+}
