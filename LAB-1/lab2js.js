63dc438fe4ea44048ffd824cc6ce0aa2f79dd377
K2adir <k2adir@gmail.com>
2023-02-15 09:45:29 -0500

    "Wild Wild Life answered"

--- LAB-1/lab2js.js
+++ LAB-1/lab2js.js
@@ -33,6 +33,16 @@ for (let i = 1; i < 101; i++) {
 // Wild Wild Life
 ////////////////////////////////
 
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+const sharky = ["Sharky", "shark", 20, "Left Coast"];
+const plantee = ["Plantee", "plant", 5000, "Mordor"];
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
+const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"];
+plantee[2]++;
+wolfy[3] = "gotham city";
+dart.push("Hawkins");
+wolfy.splice(0, 1, "gameboy");
+
 ////////////////////////////////
 // Yell at the Ninja Turtles
 ////////////////////////////////
