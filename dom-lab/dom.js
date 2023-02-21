9032c1a60093c7dcbbff34e77d298389c2339ac0
K2adir <k2adir@gmail.com>
2023-02-21 15:49:04 -0500

    commit  part 2

--- dom-lab/dom.js
+++ dom-lab/dom.js
@@ -4,10 +4,35 @@ const topMenuEl = document.getElementById("top-menu");
 // data
 var menuLinks = [
   { text: "about", href: "/about" },
-  { text: "catalog", href: "/catalog" },
-  { text: "orders", href: "/orders" },
-  { text: "account", href: "/account" },
+  {
+    text: "catalog",
+    href: "#",
+    subLinks: [
+      { text: "all", href: "/catalog/all" },
+      { text: "top selling", href: "/catalog/top" },
+      { text: "search", href: "/catalog/search" },
+    ],
+  },
+  {
+    text: "orders",
+    href: "#",
+    subLinks: [
+      { text: "new", href: "/orders/new" },
+      { text: "pending", href: "/orders/pending" },
+      { text: "history", href: "/orders/history" },
+    ],
+  },
+  {
+    text: "account",
+    href: "#",
+    subLinks: [
+      { text: "profile", href: "/account/profile" },
+      { text: "sign out", href: "/account/signout" },
+    ],
+  },
 ];
+
+// data part 2
 
 // gray background
 mainEl.style.backgroundColor = "var(--main-bg)";
@@ -32,3 +57,38 @@ menuLinks.forEach((link) => {
   menuLink.textContent = link.text;
   topMenuEl.appendChild(menuLink);
 });
+
+//// part 2
+
+const subMenuEl = document.getElementById("sub-menu");
+
+subMenuEl.style.height = "100%";
+subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
+subMenuEl.classList.add("flex-around");
+subMenuEl.style.position = "absolute";
+subMenuEl.style.top = "0";
+// data on top.
+
+var topMenuLinks = topMenuEl.getElementsByTagName("a");
+var showingSubMenu = false;
+
+topMenuEl.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.tagName !== "A") {
+    return;
+  }
+  if (event.target.classList.contains("active")) {
+    event.target.classList.remove("active");
+    showingSubMenu = false;
+    subMenuEl.style.top = "0";
+    return;
+  }
+
+  const activeLink = topMenuEl.querySelector(".active");
+  if (activeLink) {
+    activeLink.classList.remove("active");
+  }
+  event.target.classList.add("active");
+});
+
+console.log(subMenuEl);
